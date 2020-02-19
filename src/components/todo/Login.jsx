c7e9b1ca3e01bb350f68dda9eedeef0f4e98e2d2
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-02-19 17:47:59 +0545

    Login input elements are now controlled components.

--- src/components/todo/Login.jsx
+++ src/components/todo/Login.jsx
@@ -2,15 +2,41 @@ import React, { Component } from "react";
 
 class LoginComponent extends Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: 'ashish',
+            password: ''
+        }
+
+        // binding
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    }
+
     render() {
         return (
             <div>
-                <input type="text" name="username" id="username" />
-                <input type="password" name="password" id="password" />
+                <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange} />
+                <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
 
                 <button type="submit">Login</button>
             </div>
         )
+    }
+
+    handleUsernameChange(event) {
+        console.log('changed username: ' +event.target.value)
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handlePasswordChange(event){
+        this.setState({
+            password: event.target.value
+        })
     }
 }
 

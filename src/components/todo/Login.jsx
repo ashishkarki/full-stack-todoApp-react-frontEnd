762036bc6f57c901c7c154beadc336a5830dba78
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-02-19 19:45:42 +0545

    Added basic validation logic to login component for todo react app

--- src/components/todo/Login.jsx
+++ src/components/todo/Login.jsx
@@ -7,37 +7,67 @@ class LoginComponent extends Component {
 
         this.state = {
             username: 'ashish',
-            password: ''
+            password: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false
         }
 
         // binding
-        this.handleUsernameChange = this.handleUsernameChange.bind(this)
-        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleFieldChange = this.handleFieldChange.bind(this)
     }
 
     render() {
         return (
             <div>
-                <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange} />
-                <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />
+                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
+                <input type="text" name="username" value={this.state.username} onChange={this.handleFieldChange} />
+                <input type="password" name="password" value={this.state.password} onChange={this.handleFieldChange} />
 
-                <button type="submit">Login</button>
+                <button type="submit" onClick={this.loginClicked}>Login</button>
             </div>
         )
     }
 
-    handleUsernameChange(event) {
-        console.log('changed username: ' +event.target.value)
+    handleFieldChange(event) {
+        // console.log('changed field: ' + event.target.name + ' to :' + event.target.value)
         this.setState({
-            username: event.target.value
+            [event.target.name]: event.target.value
         })
     }
 
-    handlePasswordChange(event){
-        this.setState({
-            password: event.target.value
-        })
+    loginClicked = () => {
+        // no binding needed for this function since using arrow syntax
+        if (this.state.username == "ashish") {
+            console.log(this.state)
+            this.setState({
+                showSuccessMessage: true,
+                hasLoginFailed: false
+            })
+        } else {
+            this.setState({
+                showSuccessMessage: false,
+                hasLoginFailed: true
+            })
+        }
     }
+
+}
+
+function ShowInvalidCredentials(props) {
+    if (props.hasLoginFailed) {
+        return <div>Invalid credentials!!!</div>
+    } 
+
+    return null
+}
+
+function ShowLoginSuccessMessage(props) {
+    if(props.showSuccessMessage) {
+        return <div>Login Successful</div>
+    }
+
+    return null
 }
 
 export default LoginComponent
